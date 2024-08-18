5f757198d227a50f480835813673f5f21db87fb6
LagisettyRavikiran <kiranlravi8@gmail.com>
2024-08-18 23:14:09 +0530

    Update

---
+++ Day70.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode first = head;
+        ListNode second = head;
+        while(first != null && first.next != null){
+            second = second.next;
+            first = first.next.next;
+            if(second == first)
+                return true;
+        }
+        return false;
+    }
+}
